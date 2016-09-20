c466c10ed039d61a9c0357791619ec64cad2b4aa
geniuszhaoyi <genius.zhaoyi@gmail.com>
2016-09-20 09:42:36 +0800

    add a test case batch test

---
+++ Assignment.1.Tic-Tac-Toe/Tests/test.cmd
@@ -0,0 +1,12 @@
+cd ../src
+
+java Game < Tests/test1.txt
+java Game < Tests/test2.txt
+java Game < Tests/test3.txt
+java Game < Tests/test4.txt
+java Game < Tests/test5.txt
+java Game < Tests/test6.txt
+java Game < Tests/test7.txt
+java Game < Tests/test8.txt
+
+cd ../Tests
