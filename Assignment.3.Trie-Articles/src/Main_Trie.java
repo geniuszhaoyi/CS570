bc2e04cdf2a577745921785f2c9fee18125836db
zhaoyi <genius.zhaoyi@gmail.com>
2016-11-20 01:59:22 -0500

    ass3 document

--- Assignment.3.Trie-Articles/src/Main_Trie.java
+++ Assignment.3.Trie-Articles/src/Main_Trie.java
@@ -1,6 +1,6 @@
 
 public class Main_Trie {
-	public static void main(String []argv) throws Exception{
+	public static void main(String []argv){
 		System.out.println(new Ctrl(new alg_trie(), new IO1()).go());
 	}
 }
