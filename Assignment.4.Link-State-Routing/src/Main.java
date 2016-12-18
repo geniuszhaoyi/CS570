af1d65fb9e09051baf8d2e642ac4e747790ae48c
jingyu_tan <Briantanjinyu@gmail.com>
2016-12-18 17:58:17 -0500

    ASS4 T

--- Assignment.4.Link-State-Routing/src/Main.java
+++ Assignment.4.Link-State-Routing/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author TanJinyu
  *
@@ -11,10 +13,53 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Network network=new Network(new FileReader());
-		network.Continue();
-		network.ShutDown(0);
-		network.StartUp(0);
-		network.Print(0);
+		while(true){
+			System.out.println("if you want to continue enter \"C\"");
+		     System.out.println("if you want to quit enter \"Q\"");
+			System.out.println("if you want to print the routing table of a router, for example router 1 enter \"P 1\"");
+			System.out.println("if you want to shut down a router, for example router 1 enter \"S 1\"");
+			System.out.println("if you want to start up a router, for example router 1 enter \"T 1\"");
+			   Scanner scanner = new Scanner( System.in );
+			   String input = scanner.nextLine();
+			   if(input.length()==1)
+			   {
+			   if(input.equals("C"))
+			   network.Continue();
+			   else if(input.equals("Q"))
+			   break;
+			   else{
+				   System.out.println("please input the right format!");
+			   }
+			   }
+			   else if(input.length()>=3)
+			   {
+			   String[] parts = input.split(" ");
+			   String part1 = parts[0]; 
+			   String part2 = parts[1];
+			   if(part1.equals("P"))
+			   network.Print(Integer.parseInt(part2));
+			   else if(part1.equals("S"))
+			   network.ShutDown(Integer.parseInt(part2));
+			   else if(part1.equals("T"))
+			   network.StartUp(Integer.parseInt(part2));
+			   else{
+				   System.out.println("please input the right format!");
+			   }
+			   }
+			   else
+			   {
+				   System.out.println("please input the right format!");
+				   
+			   }
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+			   
+		}
 	}
 
 }
--- Assignment.4.Link-State-Routing/src/Network.java
+++ Assignment.4.Link-State-Routing/src/Network.java
@@ -15,18 +15,21 @@ public class Network {
 	
 	public void Continue(){
 		// TODO Auto-generated method stub
-		
+		System.out.println(">>continue");
 	}
 	public void Print(int routerID){
 		// TODO Auto-generated method stub
+		System.out.println(">>print "+routerID);
 		
 	}
 	public void ShutDown(int routerID){
 		// TODO Auto-generated method stub
+		System.out.println(">>shutdown "+routerID);
 		
 	}
 	public void StartUp(int routerID){
 		// TODO Auto-generated method stub
+		System.out.println(">>startup "+routerID);
 		
 	}
 }
