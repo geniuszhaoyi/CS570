f9b19cd4324abd9c569178f16c9169cb8d5d15c3
zhaoyi <genius.zhaoyi@gmail.com>
2016-12-20 15:21:41 -0500

    sfkjha iuf

--- Assignment.4.Link-State-Routing/src/FileReader.java
+++ Assignment.4.Link-State-Routing/src/FileReader.java
@@ -3,7 +3,19 @@
  *
  */
 public class FileReader {
+	class ILink{
+		int dst_routerID;
+		float link_cost;
+	}
+	class IRouter{
+		int routerID;
+		String routerName;
+		float cost;
+		ILink[] links;
+	}
 	public Router[] read_infile_dat(){
+		IRouter[] routers;
+		
 		
 		return null;
 	}
