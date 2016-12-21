159a729aceada7481cbe6124b10451d6563d6109
jingyu_tan <Briantanjinyu@gmail.com>
2016-12-21 15:27:06 -0500

    ds
    dds

--- Assignment.4.Link-State-Routing/src/NetworkPrint.java
+++ Assignment.4.Link-State-Routing/src/NetworkPrint.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
  */
 public class NetworkPrint {
 	public static void print(ArrayList<Table> tables){
-		
+		for(int i=0;i<tables.size();i++){
+			System.out.println(tables.get(i).dst_routerID+", "+tables.get(i).cost+", "+tables.get(i).outgoing_routerID);
+			
+		}
 	}
 }
